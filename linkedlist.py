782ed2807adba338d23e51cdf83497a2cfa708ee
Komorebi <chogath@qq.com>
2017-11-10 19:38:17 +0800

    链表以及一些操作

--- linkedlist.py
+++ linkedlist.py
@@ -5,6 +5,19 @@ class LinkedList:
     def __init__(self):
         self.head = Node('head')
 
+    def op(self, f):
+        head = self.head.next
+        lt = []
+        while head is not None:
+            lt.append(head.element)
+            head = head.next
+        lt = f(lt)
+
+        self.head.next = None
+        for e in lt:
+            self.append(e)
+        return self.head
+
     def append(self, data):
         next_ = self.head
         while next_.next is not None:
@@ -21,22 +34,51 @@ class LinkedList:
         return string
 
     def reverse(self):
-        head = self.head.next
-        lt = []
-        while head is not None:
-            lt.append(head.element)
-            head = head.next
-        lt = lt[::-1]
+        # 翻转
+        def f(lt):
+            lt = lt[::-1]
+            return lt
+        return self.op(f)
 
-        self.head.next = None
-        for e in lt:
-            self.append(e)
-        return self.head
+    def sort(self):
+        # 排序
+        def f(lt):
+            lt.sort()
+            return lt
+        return self.op(f)
+
+    @classmethod
+    def merge(cls, l1, l2):
+        # 合并
+        l3 = LinkedList()
+        l3.head = Node('head')
+        l1_next = l1.head.next
+        l2_next = l2.head.next
+        while True:
+            if l1_next.element < l2_next.element:
+                l3.append(l1_next.element)
+                l1_next = l1_next.next
+            else:
+                l3.append(l2_next.element)
+                l2_next = l2_next.next
+
+            if l2_next is None:
+                l3_next = l3.head
+                while l3_next.next is not None:
+                    l3_next = l3_next.next
+                l3_next.next = l1_next
+                break
+            elif l1_next is None:
+                l3_next = l3.head
+                while l3_next is not None:
+                    l3_next = l3_next.next
+                l3_next.next = l2_next
+                break
+        return l3
 
 
-def test():
+def test_reverse():
     l = LinkedList()
-    l.head = Node('head')
     l.append(111)
     l.append(222)
     l.append(333)
@@ -44,5 +86,33 @@ def test():
     assert str(l) == 'head > 333 > 222 > 111 > '
 
 
+def test_soft():
+    l = LinkedList()
+    l.append(5)
+    l.append(2)
+    l.append(1)
+    l.append(3)
+    l.sort()
+    assert str(l) == 'head > 1 > 2 > 3 > 5 > '
+
+
+def test_merge():
+    l1 = LinkedList()
+    l1.append(1)
+    l1.append(2)
+    l1.append(3)
+    l1.append(6)
+    l2 = LinkedList()
+    l2.append(2)
+    l2.append(4)
+    l2.append(5)
+    l2.append(6)
+    l3 = l1.merge(l1, l2)
+    print(l3)
+    assert str(l3) == 'head > 1 > 2 > 2 > 3 > 4 > 5 > 6 > 6 > '
+
+
 if __name__ == '__main__':
-    test()
+    test_reverse()
+    test_soft()
+    test_merge()
