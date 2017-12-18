52885e10737963864231a8e9f0fb31e6a18e2e16
Komorebi <chogath@qq.com>
2017-12-19 00:23:23 +0800

    提交

--- lzw.py
+++ lzw.py
@@ -13,17 +13,36 @@ def encoding(string):
             d[ps] = last
             last += 1
             p = s
+
+    if p != '':
+        lt.append(d[p])
+
     return lt
 
 
-def decoding():
-    pass
+def decoding(lt):
+    last = 256
+    d = {i: chr(i) for i in range(last)}
+
+    string = ''
+    p = lt.pop(0)
+    string += d[p]
+
+    for i in lt:
+        if i in d:
+            temp = d[i]
+        string += temp
+        d[last] = d[p] + temp[0]
+        last += 1
+        p = i
+    return string
 
 
 def main():
-    string = 'asdasdasd'
-    data = encoding(string)
-    print(data)
+    string = 'asd_asd_asd_asd_asd_asd_asd_asd'
+    e_data = encoding(string)
+    d_data = decoding(e_data)
+    print(e_data, d_data, string == d_data)
 
 
 if __name__ == '__main__':
